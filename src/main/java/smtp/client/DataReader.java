26722ea6a9e91e5dacef13df2ef4547562492a1b
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-11-23 14:30:01 +0100

    finish DataReader

--- src/main/java/smtp/client/DataReader.java
+++ src/main/java/smtp/client/DataReader.java
@@ -2,39 +2,108 @@ package smtp.client;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 public abstract class DataReader {
-    public static ArrayList<String> getAddressesFromFile(String path) throws IOException, ParseException {
-        Object object = new JSONParser().parse(new FileReader(path));
-        JSONObject jo = (JSONObject) object;
-        JSONArray ja = (JSONArray) jo.get("addresses");
-        ArrayList<String> addresses = new ArrayList<>();
+    /**
+     * Regex to validate email addresses
+     * Taken from <a href="https://stackoverflow.com/questions/8204680/java-regex-email/13013061#13013061">...</a>
+     */
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-        for (Object o : ja) {
-            JSONObject message = (JSONObject) o;
-            addresses.add((String) message.get("address"));
-        }
-
-        return addresses;
+    /**
+     * Validates an email address
+     *
+     * @param address the address to validate
+     * @return true if the address is valid, false otherwise
+     */
+    private static boolean validateAddress(String address) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(address);
+        return matcher.matches();
     }
 
-    public static ArrayList<Message> getMessagesFromFile(String path) throws IOException, ParseException {
-        Object object = new JSONParser().parse(new FileReader(path));
-        JSONObject jo = (JSONObject) object;
-        JSONArray ja = (JSONArray) jo.get("messages");
-        ArrayList<Message> messages = new ArrayList<>();
+    /**
+     * Reads a file containing email addresses and returns them as an ArrayList
+     * @param path the path to the file
+     * @return an ArrayList containing the email addresses
+     * @throws IOException if the file is not found
+     * @throws ParseException if the file is not in the correct format
+     * @throws RuntimeException if the file contains invalid addresses
+     */
+    public static ArrayList<String> getAddressesFromFile(String path) throws IOException, ParseException {
+        try (FileReader fileReader = new FileReader(path)) {
+            Object object = new JSONParser().parse(fileReader);
+            JSONObject jo = (JSONObject) object;
+            JSONArray ja = (JSONArray) jo.get("addresses");
+            if (ja == null) {
+                throw new ParseException(0);
+            }
+            ArrayList<String> addresses = new ArrayList<>();
 
-        for (Object o : ja) {
-            JSONObject message = (JSONObject) o;
-            String subject = (String) message.get("subject");
-            String body = (String) message.get("body");
-            messages.add(new Message(subject, body));
+            for (Object o : ja) {
+                JSONObject message = (JSONObject) o;
+                String address = (String) message.get("address");
+                if (address == null) {
+                    throw new ParseException(0);
+                }
+                if (!validateAddress(address)) {
+                    throw new RuntimeException();
+                }
+                addresses.add(address);
+            }
+
+            return addresses;
+        } catch (IOException e) {
+            System.out.println("Erreur : fichier introuvable");
+        } catch (ParseException e) {
+            System.out.println("Erreur : le fichier n'est pas au bon format");
+        } catch (RuntimeException e) {
+            System.out.println("Erreur : le fichier contient des adresses invalides");
         }
 
-        return messages;
+        return null;
+    }
+
+    /**
+     * Reads a file containing messages and returns them as an ArrayList
+     * @param path the path to the file
+     * @return an ArrayList containing the messages
+     * @throws IOException if the file is not found
+     * @throws ParseException if the file is not in the correct format
+     */
+    public static ArrayList<Message> getMessagesFromFile(String path) throws IOException, ParseException {
+        try (FileReader fileReader = new FileReader(path)) {
+            Object object = new JSONParser().parse(fileReader);
+            JSONObject jo = (JSONObject) object;
+            JSONArray ja = (JSONArray) jo.get("messages");
+            if (ja == null) {
+                throw new ParseException(0);
+            }
+            ArrayList<Message> messages = new ArrayList<>();
+
+            for (Object o : ja) {
+                JSONObject message = (JSONObject) o;
+                String subject = (String) message.get("subject");
+                String body = (String) message.get("body");
+                if (subject == null || body == null) {
+                    throw new RuntimeException();
+                }
+                messages.add(new Message(subject, body));
+            }
+
+            return messages;
+        } catch (IOException e) {
+            System.out.println("Erreur : le fichier introuvable");
+        } catch (ParseException | RuntimeException e) {
+            System.out.println("Erreur : le fichier n'est pas au bon format");
+        }
+
+        return null;
     }
 }
